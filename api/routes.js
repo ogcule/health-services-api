64535df45e8852e0f452957a40729a86e60bfd60
ogcule <ogcule@outlook.com>
2017-11-01 07:35:51 +0000

    update

--- api/routes.js
+++ api/routes.js
@@ -1,24 +1,31 @@
-import bodyParser from 'body-parser';
+
 import {getAllServices, getSingleService, createService, updateService, removeService} from './hsControllers';
 import {getAllFaq, getFaq, createFaq, updateFaq, removeFaq} from './faqControllers.js';
-// Parse body of incoming request with body-parser
-const jsonParser = bodyParser.json()
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
+import { check, validationResult } from 'express-validator/check';
+import { matchedData, sanitize } from 'express-validator/filter';
 
 const routes = (app) => {
   app.get('/service', getAllServices);
   app.get('/service/:id', getSingleService);
-  app.post('/service', urlencodedParser, createService);
-  app.put('/service/:id',urlencodedParser, updateService);
+  app.post('/service', createService);
+  app.put('/service/:id', updateService);
   app.delete('/service/:name', removeService);
 
   app.get('/faq', getAllFaq);
   app.get('/faq/:id', getFaq);
-  app.post('/faq', urlencodedParser, createFaq);
-  app.put('/faq/:id',urlencodedParser, updateFaq);
+  app.post('/faq',[
+    check('question', 'Please enter a question').isLength({ min: 1 }),
+    check('answer','Please enter an answer').isLength({ min: 1 })
+  ],(req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.mapped().answer.msg);
+    return res.status(422).json({ error: errors.mapped() });
+  }
+  next();
+}, createFaq);
+  app.put('/faq/:id', updateFaq);
   app.delete('/faq/:id', removeFaq);
-
-
 }
 
 export default routes;
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import expressValidator from 'express-validator';
 import routes from './api/routes';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
+// error handler
 app.use(function(err, req, res, next) {
   res.status(err.status || 500)
   .json({
