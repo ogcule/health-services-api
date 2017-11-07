7779625f00393f54e6b1ac3a32dfba8b7f677c2a
ogcule <ogcule@outlook.com>
2017-11-07 00:08:21 +0000

    update

--- services.sql
+++ services.sql
@@ -1,7 +1,9 @@
 DROP DATABASE IF EXISTS services;
 CREATE DATABASE services;
-CREATE EXTENSION IF NOT EXISTS citext;
+
 \c services;
+
+CREATE EXTENSION IF NOT EXISTS citext;
 
 CREATE TABLE service (
   ID SERIAL PRIMARY KEY,
@@ -12,10 +14,11 @@ CREATE TABLE service (
   email citext NOT NULL UNIQUE,
   category VARCHAR NOT NULL,
   description VARCHAR NOT NULL,
-  link VARCHAR NOT NULL
+  link VARCHAR NOT NULL,
+  rcgp_curriculum VARCHAR NOT NULL
 );
 
-INSERT INTO service (image, name, address, telephone, email, category, description, link)
+INSERT INTO service (image, name, address, telephone, email, category, description, link, rcgp_curriculum)
   VALUES (
     'link to image goes here',
    'Primary Care Mental Health Team (Improving Access to Psychological Therapies or IAPT)',
@@ -25,5 +28,6 @@ INSERT INTO service (image, name, address, telephone, email, category, descripti
    'email@gmail.com',
    'Mental Health',
    'A team of trained mental health psychological wellbeing practitioners and therapists offering a wide range of treatments and interventions to help those experiencing common mental health problems',
-    'weblink goes here'
+    'weblink goes here',
+    ' Care of people with mental health problems'
   );
